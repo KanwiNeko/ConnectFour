a763a095ed1ee5925ebe6b7a77faf2d98d411fbb
kanwiNeko <katherine.chestmon@outlook.com>
2022-11-22 18:18:59 -0500

    first "entrega3" commit

--- Connect4.py
+++ Connect4.py
@@ -1,9 +1,11 @@
 import random
 import os
 
-#for coloring inside the code
+# for coloring inside the code
 os.system("")
-class style():
+
+
+class Style:
     BLACK = '\033[30m'
     RED = '\033[31m'
     GREEN = '\033[32m'
@@ -15,10 +17,11 @@ class style():
     UNDERLINE = '\033[4m'
     RESET = '\033[0m'
 
+
 class Player:
     def __init__(self, iden):
         self.name = str(input(f"Hola jugador {str(iden+1)}, porfavor indica tu nombre: "))
-        self.Token = None
+        self.token = None
         self.iden = iden
 
 
@@ -38,17 +41,17 @@ def game():
 
     for i in range(2):
         player[i] = Player(i)
-    player[0].Token = tokencall(player[0].name)
+    player[0].token = tokencall(player[0].name)
 
-    if player[0].Token == "X":
-        player[1].Token = "O"
-    elif player[0].Token == "O":
-        player[1].Token = "X"
+    if player[0].token == "X":
+        player[1].token = "O"
+    elif player[0].token == "O":
+        player[1].token = "X"
 
-    print(f"{player[1].name}, vas a jugar con la ficha [{str(player[1].Token)}] \n")
+    print(f"{player[1].name}, vas a jugar con la ficha [{str(player[1].token)}] \n")
     print("Lanzando una moneda al aire para determinar quién inicia la partida...")
-    currentPlayer = random.randint(0, 1)
-    print(f"la partida la inicia {player[currentPlayer].name} \n")
+    current_player = random.randint(0, 1)
+    print(f"la partida la inicia {player[current_player].name} \n")
 
     def printboard():
         def printlines():
@@ -58,45 +61,43 @@ def game():
             print("+")
         print("")
         print("          1234567 ")
-        #print the line on the top of the board
+        # print the line on the top of the board
         printlines()
         for i in range(len(grid)):
             print("         |", end="")
             for j in range(len(grid[0])):
                 if grid[i][j] == "X":
-                    print(style.RED + str(grid[i][j]) + style.RESET, end="")
+                    print(Style.RED + str(grid[i][j]) + Style.RESET, end="")
                 elif grid[i][j] == "O":
-                    print(style.BLUE + str(grid[i][j]) + style.RESET, end="")
+                    print(Style.BLUE + str(grid[i][j]) + Style.RESET, end="")
                 else:
                     print(grid[i][j], end="")
-                #print(grid[i][j], end="")
             print("|")
-        #print the line on the bottom of the board
+        # print the line on the bottom of the board
         printlines()
         print("")
 
     roundid = 0
-    colum = 0
-    myinput = None
 
-    while roundid <= ((len(grid)*len(grid[0]))):
+    while roundid <= (len(grid)*len(grid[0])):
         os.system('cls' if os.name == 'nt' else 'clear')
         myinput = None
         colum = 0
         printboard()
-        while myinput == None:
-            print(f"{style.GREEN}{player[currentPlayer].name}{style.RESET}, indica un número de columna o introduce [S] para tentar a la suerte: ", end="")
+        while myinput is None:
+            print(f"{Style.GREEN}{player[current_player].name}{Style.RESET}, indica un número de columna o introduce "
+                  f"[S] para tentar a la suerte: ", end="")
             myinput = str(input())
             if myinput.isnumeric():
-                if 1 <= int(myinput) <= 8:
+                if 1 <= int(myinput) < 8:
                     colum = int(myinput)
                     break
                 else:
                     continue
-            elif myinput == "S" or myinput == "s":
-                colum = random.randint(1,7)
+            elif myinput.capitalize() == "S":
+                colum = random.randint(1, 7)
                 while grid[0][colum-1] != "•":
-                    colum = random.randint(1,7)
+                    colum = random.randint(1, 7)
                 break
             else:
                 continue
@@ -107,19 +108,20 @@ def game():
         for i in range(len(grid)):
             if grid[len(grid)-i-1][colum-1] == "•":
                 print("the spot is empty")
-                grid[len(grid)-i-1][colum-1] = player[currentPlayer].Token
+                grid[len(grid)-i-1][colum-1] = player[current_player].token
                 break
-        currentPlayer = (currentPlayer + 1) % 2
+        current_player = (current_player + 1) % 2
         roundid += 1
     playagain = None
-    while playagain == None:
+    while playagain is None:
         print("Desea jugar otra vez? [Y]/[N]: ")
         sto1 = input()
-        if sto1 == "y" or sto1 == "y":
+        if sto1.capitalize() == "y":
             playagain = True
-        elif sto1 == "N" or sto1 == "n":
+        elif sto1.capitalize() == "N":
             playagain = False
     if playagain:
         game()
+
 
 game()
